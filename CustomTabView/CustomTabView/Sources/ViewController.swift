52bddeb0d68d8105a72aa5c0c6539ff5dc568903
MykolaZabrotskyi <zabrotskyi.mykola@gmail.com>
2026-03-02 14:31:47 +0200

    Initializes new iOS project
    Establishes the foundational Xcode project structure and boilerplate code for the CustomTabView application.
    
    Includes:
    - Project file and workspace configuration
    - Standard AppDelegate and SceneDelegate
    - Base ViewController
    - Essential asset catalogs, Info.plist, and LaunchScreen.storyboard
    
    Provides a clean and ready-to-use setup for further development.

---
+++ CustomTabView/CustomTabView/Sources/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CustomTabView
+//
+//  Created by Mykola Zabrotskyi on 02.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
